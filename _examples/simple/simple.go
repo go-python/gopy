df3502d45c57915194051ced21d74aba674cd6bf
Sebastien Binet <binet@cern.ch>
2015-08-06 17:57:00 +0200

    examples: add a very simple package for benchmarking
    Change-Id: I23fd2b578a8c2217e96f9e3e2f879f2a43ece93d

---
+++ _examples/simple/simple.go
@@ -0,0 +1,9 @@
+// Copyright 2015 The go-python Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// simple is a simple package.
+package simple
+
+// Func is a simple func
+func Func() {}
