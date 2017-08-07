76264b4453f1c068e1a706e2c959ee492a767c14
Sebastien Binet <binet@cern.ch>
2017-08-07 15:29:31 +0200

    gopy: add a simple test for strings

---
+++ _examples/gostrings/test.py
@@ -0,0 +1,11 @@
+# Copyright 2017 The go-python Authors.  All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+## py2/py3 compat
+from __future__ import print_function
+
+import gostrings
+
+print("S1 = %s" % (gostrings.GetS1(),))
+print("GetString() = %s" % (gostrings.GetString(),))
