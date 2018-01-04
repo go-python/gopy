cd86165f6363a3d5caec181c163725d5906abbfb
Ramakrishnan G <rameshg87@gmail.com>
2018-01-05 01:04:20 +0530

    gopy: refactor main to assess coverage
    - Refactor main() to be able to invoke with custom args from the test.
    - Use run() method in unit test to make it possible to assess coverage
      using go test.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -31,19 +32,24 @@ func init() {
 	}
 }
 
+func run(args []string) error {
+	err := app.Flag.Parse(args)
+	if err != nil {
+		return fmt.Errorf("could not parse flags: %v", err)
+	}
+
+	appArgs := app.Flag.Args()
+	err = app.Dispatch(appArgs)
+	if err != nil {
+		return fmt.Errorf("error dispatching command: %v", err)
+	}
+	return nil
+}
+
 func main() {
-	err := app.Flag.Parse(os.Args[1:])
+	err := run(os.Args[1:])
 	if err != nil {
-		log.Printf("error parsing flags: %v\n", err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
-
-	args := app.Flag.Args()
-	err = app.Dispatch(args)
-	if err != nil {
-		log.Printf("error dispatching command: %v\n", err)
-		os.Exit(1)
-	}
-
 	os.Exit(0)
 }
