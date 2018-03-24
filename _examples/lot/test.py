0db6ee7793bc92d125c8b762dbe27b02011d6c01
Dong-hee Na <donghee.na92@gmail.com>
2018-03-24 13:30:12 +0000

    cffi: Fix invalid syntax for slices in generated Python

---
+++ _examples/lot/test.py
@@ -0,0 +1,14 @@
+# Copyright 2018 The go-python Authors.  All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+import lot
+
+l = lot.New()
+print('l.SomeString : {}'.format(l.SomeString))
+print('l.SomeInt : {}'.format(l.SomeInt))
+print('l.SomeFloat : {}'.format(l.SomeFloat))
+print('l.SomeBool : {}'.format(l.SomeBool))
+print('l.SomeListOfStrings: {}'.format(l.SomeListOfStrings))
+print('l.SomeListOfInts: {}'.format(l.SomeListOfInts))
+print('l.SomeListOfFloats: {}'.format(l.SomeListOfFloats))
+print('l.SomeListOfBools: {}'.format(l.SomeListOfBools))
