7dfc7fde5f6c1fae8e17d7b18e01233461bdbd09
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2019-02-12 23:21:48 -0700

    build / makefile fixes -- back to .so for darwin

---
+++ main_darwin.go
@@ -0,0 +1,12 @@
+// Copyright 2019 The go-python Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build darwin
+
+package main
+
+const (
+	// libExt = ".dylib"  // theoretically should be this but python only recognizes .so
+	libExt = ".so"
+)
