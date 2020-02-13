1a93451c5c7887306767bd4a7effced84ed7ccb2
Cosmos Nicolaou <cosmos.nicolaou@gmail.com>
2020-02-13 08:10:00 -0800

    .

--- _examples/cstrings/test.py
+++ _examples/cstrings/test.py
@@ -2,12 +2,17 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-## py2/py3 compat
+# py2/py3 compat
 from __future__ import print_function
 
 import cstrings
+import sys
+import gc
 
 v = cstrings.StringValue()
-
+print(sys.getrefcount(v))
 print(v)
+print(gc.is_tracked(v))
+
+del v
 print("OK")
